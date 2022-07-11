b8486fc34bd5e2126834cc5c5b3b6195f2a24afe
Tobias van Beek <t.vanbeek@tjvb.nl>
2022-07-11 13:52:40 +0200

    Add tests for the PipelineHookHandler to catch a mutation found by infection.

--- tests/Services/PipelineHookHandlerTest.php
+++ tests/Services/PipelineHookHandlerTest.php
@@ -57,4 +57,191 @@ class PipelineHookHandlerTest extends TestCase
         $this->assertNotEmpty($pipelineUpdateService->receivedData);
         $this->assertNotEmpty($projectUpdateService->receivedData);
     }
+
+    /**
+     * @test
+     */
+    public function weDontStoreInvalidPipelineData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        $hookModel->body['object_attributes'] = 'invalid pipeline';
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertNotEmpty($buildUpdateService->receivedData);
+        $this->assertNotEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertEmpty($pipelineUpdateService->receivedData);
+        $this->assertNotEmpty($projectUpdateService->receivedData);
+    }
+
+    /**
+     * @test
+     */
+    public function weDoNotStoreInvalidProjectData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        $hookModel->body['project'] = 'invalid projectdata';
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertNotEmpty($buildUpdateService->receivedData);
+        $this->assertNotEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertNotEmpty($pipelineUpdateService->receivedData);
+        $this->assertEmpty($projectUpdateService->receivedData);
+    }
+
+    /**
+     * @test
+     */
+    public function weDoNotStoreInvalidMergeRequestData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        $hookModel->body['merge_request'] = 'invalid merge_request data';
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertNotEmpty($buildUpdateService->receivedData);
+        $this->assertEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertNotEmpty($pipelineUpdateService->receivedData);
+        $this->assertNotEmpty($projectUpdateService->receivedData);
+    }
+
+    /**
+     * @test
+     */
+    public function weDoNotStoreInvalidBuildData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        $hookModel->body['builds'] = ['invalid build data'];
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertEmpty($buildUpdateService->receivedData);
+        $this->assertNotEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertNotEmpty($pipelineUpdateService->receivedData);
+        $this->assertNotEmpty($projectUpdateService->receivedData);
+    }
+
+    /**
+     * @test
+     */
+    public function weDoNotStopForInvalidBuildData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        // add an invalid build in front of the others
+        array_unshift($hookModel->body['builds'], 'invalid build data');
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertNotEmpty($buildUpdateService->receivedData);
+        $this->assertNotEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertNotEmpty($pipelineUpdateService->receivedData);
+        $this->assertNotEmpty($projectUpdateService->receivedData);
+    }
+
+    /**
+     * @test
+     */
+    public function weDoNotStoreInvalidBuildsData(): void
+    {
+        // setup / mock
+        $hookModel = new FakeGitLabHookModel();
+        $hookModel->body = \Safe\json_decode(\Safe\file_get_contents(self::EXAMPLE_PAYLOADS . 'pipeline.json'), true);
+        $hookModel->objectKind = $hookModel->eventType = $hookModel->eventName = 'pipeline';
+        $hookModel->body['builds'] = 'invalid builds data';
+        $buildUpdateService = new FakeBuildUpdateService();
+        $mergeRequestUpdateService = new FakeMergeRequestUpdateService();
+        $pipelineUpdateService = new FakePipelineUpdateService();
+        $projectUpdateService = new FakeProjectUpdateService();
+
+        // run
+        $handler = new PipelineHookHandler(
+            $buildUpdateService,
+            $mergeRequestUpdateService,
+            $pipelineUpdateService,
+            $projectUpdateService
+        );
+        $handler->handle($hookModel);
+
+        // verify/assert
+        $this->assertEmpty($buildUpdateService->receivedData);
+        $this->assertNotEmpty($mergeRequestUpdateService->receivedData);
+        $this->assertNotEmpty($pipelineUpdateService->receivedData);
+        $this->assertNotEmpty($projectUpdateService->receivedData);
+    }
 }
