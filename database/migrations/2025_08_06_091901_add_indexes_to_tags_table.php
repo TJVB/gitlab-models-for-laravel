b40a55392ce5f7aea3f85aa5d01d45fe0f3f886a
Tobias van Beek <t.vanbeek@tjvb.nl>
2025-08-06 11:40:19 +0200

    Add indexes to the fields in the tables that are used in the repository to find the records if they exist.
    This will speed up the database queries to check if a record existing before writing or updating.

---
+++ database/migrations/2025_08_06_091901_add_indexes_to_tags_table.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('gitlab_tags', static function (Blueprint $table): void {
+            $table->unsignedBigInteger('project_id')->index()->change();
+            $table->string('ref')->index()->change();
+            $table->softDeletes()->index()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('gitlab_tags', static function (Blueprint $table): void {
+            $table->dropIndex(['deleted_at']);
+            $table->dropIndex(['ref']);
+            $table->dropIndex(['project_id']);
+        });
+    }
+};
---
+++ database/migrations/2025_08_06_093339_add_indexes_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('gitlab_users', static function (Blueprint $table): void {
+            $table->unsignedBigInteger('user_id')->index()->change();
+            $table->softDeletes()->index()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('gitlab_users', static function (Blueprint $table): void {
+            $table->dropIndex(['deleted_at']);
+            $table->dropIndex(['user_id']);
+        });
+    }
+};
